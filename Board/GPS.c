95ed7d7b0877b40168e936ab8fbaa4e48fc01591
Pat S <patrick@satyshur.net>
2013-12-11 22:47:03 -0600

    Initial commit

---
+++ Board/GPS.c
@@ -0,0 +1,42 @@
+/*   This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+*	\brief		GPS Functions.
+*	\author		Pat Satyshur
+*	\version	1.0
+*	\date		12/11/2013
+*	\copyright	Copyright 2013, Pat Satyshur
+*	\ingroup 	hardware
+*
+*	@{
+*/
+
+#include "main.h"
+
+/** Returns 0 if the device initalizes correctly, 0xFF if not */
+uint8_t GPS_Init(void)
+{
+	
+	return 0x00;
+}
+
+void GPS_SetBias(uint8_t Bias)
+{
+	return;
+}
+
+#endif
+
+/** @} */
---
+++ Board/GPS.h
@@ -0,0 +1,39 @@
+/*   This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+*	\brief		GPS Function header file.
+*	\author		Pat Satyshur
+*	\version	1.0
+*	\date		12/11/2013
+*	\copyright	Copyright 2013, Pat Satyshur
+*	\ingroup 	hardware
+*
+*	@{
+*/
+
+#ifndef _GPS_H_
+#define _GPS_H_
+
+#include "stdint.h"
+
+//Setup the CS pin
+#define GPS_BIAS_PORT	PORTB
+#define BPS_BIAS_PIN	0
+
+uint8_t GPS_Init(void);
+void GPS_SetBias(uint8_t Bias);
+
+#endif
+/** @} */
